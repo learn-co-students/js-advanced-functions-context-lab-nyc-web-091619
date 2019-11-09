04f70fce620f30cac99a9795ee4e508f1e57b084
Altaf Quadri <altafmquadri@gmail.com>
2019-11-09 17:14:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,69 @@
 /* Your Code Here */
+function createEmployeeRecord(employee) {
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map(function(employee){
+        return createEmployeeRecord(employee)
+    })
+}
+
+function createTimeInEvent(date) {
+    let obj = {
+        type: "TimeIn",
+        hour: parseInt(date.split(' ')[1].split(':')[0]),
+        date: date.split(' ')[0]
+    }
+    this.timeInEvents.push(obj)
+    return this
+}
+
+function createTimeOutEvent(date) {
+    let obj = {
+        type: "TimeOut",
+        hour: parseInt(date.split(' ')[1].split(':')[0]),
+        date: date.split(' ')[0]
+    }
+    this.timeOutEvents.push(obj)
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    
+    let timeIn = this.timeInEvents.find(function(eTIE){
+        return eTIE.date === date
+    })
+
+    let timeOut = this.timeOutEvents.find(function(eTIE){
+        return eTIE.date === date
+    })
+
+    return (timeOut.hour - timeIn.hour)/100
+}
+
+function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+function findEmployeeByFirstName(employees, name) {
+    return employees.find(function(employee){
+        return employee.firstName === name
+    })
+}
+
+function calculatePayroll(employees) {
+    return employees.reduce(function(sum, employee){
+        return sum + allWagesFor.call(employee)
+    },0)
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,3 +85,4 @@ let allWagesFor = function () {
 
     return payable
 }
+
